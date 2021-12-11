5f55a5ca05f4688a8d79b97dd1cc0ed2fec70a06
jcabrera <julian.cabreras@gmail.com>
2021-12-10 20:11:53 -0600

    refactor of kafka 2

--- app/udaconnect/services.py
+++ app/udaconnect/services.py
@@ -8,7 +8,6 @@ from app.udaconnect.schemas import LocationSchema
 from geoalchemy2.functions import ST_Point
 import json
 from sqlalchemy.sql import text
-from controllers import g
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-api")
@@ -97,7 +96,7 @@ class PersonService:
         KAFKA_SERVER = 'localhost:9092'
         producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
         kafka_data = json.dumps(new_person).encode()
-        kafka_producer = g.kafka_producer
+        kafka_producer = producer
         kafka_producer.send("persona", kafka_data)
 
         return new_person
